de5320a2e028a331f02d720acdecee63bf1ca84f
li_yun <li_yun@ubuntu.(none)>
2013-10-31 17:45:49 +0800

    add hello

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from mysite.views import hello
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^hello/$', hello),
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
