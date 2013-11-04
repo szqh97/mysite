bece08a331610be323f54cc7ebd97f6912286cba
li_yun <li_yun@ubuntu.(none)>
2013-11-04 18:14:50 +0800

    add new for forms

--- books/views.py
+++ books/views.py
@@ -1,1 +1,51 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail
+from models import *
+from django.views.decorators.csrf import csrf_protect
+
+def search_form(request):
+    return render_to_response('search_form.html')
+
+def display_meta(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k,v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table> %s </table>' %'\n'.join(html))
+
+def search(request):
+    error = False
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            error = True
+        elif len(q) > 20:
+            error = True
+        else:
+            books = Book.objects.filter(title__icontains = query)
+            return render_to_response('search_results.html', locals())
+    return render_to_response('search_form.html', locals())
+
+@csrf_protect
+def contact(request):
+    errors = []
+    if request.method == 'POST':
+        if not request.POST.get('subject', ''):
+            errors.append('Enter a subject.')
+        if not request.POST.get('message', ''):
+            errors.append('Enter a message.')
+        if request.POST.get('e-mail') and '@' not in request.POST['e-mail']:
+            errors.append('Enter a valid e-mail address.')
+        if not errors:
+            send_mail(
+                request.POST['subject'],
+                request.POST['message'],
+                request.POST.get('e-mail', 'li_yun@vobile.cn'),
+                ['szqh97@163.com'],
+            )
+            #return HttpResponseRedirect('/contact/thanks/')
+            return HttpResponseRedirect('hello.html')
+    return render_to_response('contact_form.html', RequestContext({'errors': errors}))
+
--- mysite/views.py
+++ mysite/views.py
@@ -5,9 +5,26 @@ from django.shortcuts import render_to_response
 from django.template.loader import get_template
 
 def hello(request):
-    return "hello"
+    #return HttpResponse("path: %s, host: %s, full_path: %s, is_secure: %s" %  (request.path, request.get_host(), request.get_full_path(), request.is_secure()) )
+    t = display_meta(request)
+    return t
 
 def current_datetime(request):
     now = datetime.datetime.now()
     title = "KKK"
     return render_to_response('mypage.html', locals())
+
+def display_meta(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k,v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table> %s </table>' %'\n'.join(html))
+
+def search(request):
+    if 'q' in request.GET:
+        message = 'You searched for: %r' % request.GET['q']
+    else:
+        message = 'You submitted an empty form.'
+    return HttpResponse(message)
